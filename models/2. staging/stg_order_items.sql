c5f15d30e2faac41992541d489f26b9e87cbbb32
Thành Long Lê <79347364+AnthonyLe632@users.noreply.github.com>
2023-11-25 12:29:56 +0700

    Shorten headers naming and update yaml dictionary

--- models/2. staging/stg_order_items.sql
+++ models/2. staging/stg_order_items.sql
@@ -5,11 +5,11 @@ stg_order_items AS (
     SELECT
         CAST(order_id AS {{ dbt.type_string() }}) AS order_id
         , CAST(order_item_id AS {{ dbt.type_int() }}) AS order_item_id
-        , CAST(product_id AS {{ dbt.type_string() }}) AS product_id
+        , CAST(product_id AS {{ dbt.type_string() }}) AS prod_id
         , CAST(seller_id AS {{ dbt.type_string() }}) AS seller_id
-        , CAST(shipping_limit_date AS timestamp) AS shipping_limit_date
-        , CAST(shipping_limit_date AS DATE) AS extracted_date
-        , CAST(shipping_limit_date AS time) AS extracted_time
+        , CAST(shipping_limit_date AS timestamp) AS ship_lim_datetime
+        , CAST(shipping_limit_date AS DATE) AS ship_lim_date
+        , CAST(shipping_limit_date AS time) AS ship_lim_time
         , CAST(price AS {{ dbt.type_float() }}) AS item_price
         , CAST(freight_value as {{ dbt.type_float() }}) AS freight_value
     FROM raw_order_items 
--- models/2. staging/stg_order_reviews.sql
+++ models/2. staging/stg_order_reviews.sql
@@ -3,13 +3,13 @@ WITH raw_order_reviews AS (SELECT * FROM {{ ref('raw_order_reviews') }}),
 stg_order_reviews AS (
 
     SELECT
-        CAST(review_id AS {{ dbt.type_string() }}) AS review_id
+        CAST(review_id AS {{ dbt.type_string() }}) AS rev_id
         , CAST(order_id AS {{ dbt.type_string() }}) AS order_id
-        , CAST(review_score AS {{ dbt.type_int() }}) AS review_score
-        , CAST(review_comment_title AS {{ dbt.type_string() }}) AS review_comment_title
-        , CAST(review_comment_message AS {{ dbt.type_string() }}) AS review_comment_message
-        , CAST(review_creation_date AS timestamp) AS review_creation_date
-        , CAST(review_answer_timestamp AS timestamp) AS review_answer_timestamp
+        , CAST(review_score AS {{ dbt.type_int() }}) AS rev_score
+        , CAST(review_comment_title AS {{ dbt.type_string() }}) AS rev_cmt_title
+        , CAST(review_comment_message AS {{ dbt.type_string() }}) AS rev_cmt_message
+        , CAST(review_creation_date AS timestamp) AS rev_creation_date
+        , CAST(review_answer_timestamp AS timestamp) AS rev_answer_timestamp
     FROM raw_order_reviews
 
 )
